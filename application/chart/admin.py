61154e207525dea2a7f707c8f0b6828ab0abefca
jialinzou <jzou90s@gmail.com>
2016-06-30 13:31:20 -0400

    remove old folders

--- application/chart/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models.chart import MonthByMonth, MonthView, Channel
-
-admin.site.register(MonthByMonth)
-admin.site.register(MonthView)
-admin.site.register(Channel)
