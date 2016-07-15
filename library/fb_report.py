fe95569fecb21b2edbeda5f07a73b900ada8ee60
jialinzou <jzou90s@gmail.com>
2016-07-15 16:11:28 -0400

    update facebook library

--- library/fb_report.py
+++ library/fb_report.py
@@ -4,17 +4,27 @@ from urllib import request
 
 access_token = json.load(open("fb_tokens"))
 
-def get_video_views():
-	today = dt.date.today()
-	delta = today.isoweekday() % 7 
-	last_week = today - dt.timedelta(days = delta)
-	last_last_week = last_week - dt.timedelta(days = 8)
+def get_post_ids(brand, next_page=None):  
+    if next_page:
+        requestUrl = next_page
+    else:
+        last_24h = dt.datetime.now() - dt.timedelta(hours = 24)
+        start = str(int((last_24h-dt.datetime(1970,1,1)).total_seconds()))
+        endpoint = "https://graph.facebook.com/v2.5/me/posts?"
+        fields = 'id,created_time' 
+        requestUrl = endpoint + 'fields='+fields + "&access_token=" + access_token[brand] + '&since=' + start
+    req = request.Request(requestUrl)
+    response = request.urlopen(req)
+    result = json.loads(response.read().decode("utf8"))
+    if 'paging' in result:
+        result['data'] += get_post_ids(brand, next_page=result['paging']['next'])
+    return result['data']
 
-	endpoint = "https://graph.facebook.com/v2.5/me/insights/page_video_views_organic?period=week"
-
-	requestUrl = endpoint + "&access_token=" + access_token["MH"] + "&since=" + str(last_last_week) + "&until=" + str(last_week)
-
-	req = request.Request(requestUrl)
-	response = request.urlopen(req)
-	data = json.loads(response.read().decode("utf8"))
-	return data
+def get_viral_unique(post_id, brand):
+    endpoint = 'https://graph.facebook.com/v2.7/'
+    fields = '/insights/post_impressions_viral_unique'
+    requestUrl = endpoint + post_id + fields + '?access_token=' + access_token[brand]
+    req = request.Request(requestUrl)
+    response = request.urlopen(req)
+    data = json.loads(response.read().decode("utf8"))    
+    return data
--- scripts/fetch_google_data.py
+++ scripts/fetch_google_data.py
@@ -7,8 +7,9 @@ def run():
 		'apogee': '123191441', #apogee
 		'MH': '116151777' #men's health
 	}
+	today = str(datetime.date.today())
 	analytics = initialize_analyticsreporting()
-	response = get_report(analytics, view_id['MH'], '2016-07-13')
+	response = get_report(analytics, view_id['MH'], today)
 	rows = response['reports'][0]['data']['rows']
 	time = datetime.datetime.strptime(rows[0]['dimensions'][0], '%Y%m%d')
 	users = {'(Other)':0, 'Direct':0, 'Email':0, 'Organic Search':0, 
