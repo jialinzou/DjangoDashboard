b77b1b291e1697a1882d304996949a11391fc565
Jialin Zou <jzou90s@gmail.com>
2016-07-24 12:48:18 -0400

    remove python script for get concurrents

--- application/chart/urls.py
+++ application/chart/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     url(r'^get_users_per_channel', GetUsersPerChannle.as_view(), name="get_users_per_channel"),
     url(r'^get_top_pages', GetTopPages.as_view(), name="get_top_pages"),
     url(r'^get_top_posts', GetTopPosts.as_view(), name="get_top_posts"),
-    url(r'^get_concurrents', GetConcurrents.as_view(), name="get_concurrents"),
     ]
